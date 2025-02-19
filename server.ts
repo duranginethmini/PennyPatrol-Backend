5db193197846a842ac61f13da7ec95e6701e233a
duranginethmini <durangineth02@gmail.com>
2025-02-19 21:52:57 +0530

    Initialized and set up the project

---
+++ server.ts
@@ -0,0 +1,33 @@
+import express, { Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+import userRouter from "./routes/userRouter";
+import cors from "cors";
+import errorHandler from "./middlewares/errorHandlerMiddleware";
+import categoryRouter from "./routes/categoryRouter";
+import transactionRouter from "./routes/transactionRouter";
+
+dotenv.config();
+const app = express();
+
+//! Cors config
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+    })
+);
+
+//! Middlewares
+app.use(express.json()); // Pass incoming JSON data
+
+//! Routes
+app.use(userRouter);
+app.use(categoryRouter);
+app.use(transactionRouter)
+
+//! Error handler
+app.use(errorHandler);
+
+// Start server
+app.listen(8000, () => {
+    console.log("Server running on port 8000");
+});
